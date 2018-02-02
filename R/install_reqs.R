1cb3aedc29f2b85772c9cf336294ff944b839b27
abelcastilloavant <abel.castillo@avant.com>
2018-02-02 16:26:36 -0600

    better function names

--- R/install_reqs.R
+++ R/install_reqs.R
@@ -1,16 +1,16 @@
-lockbox <- function(requirements_file, lockfile_path, save_tree = TRUE) {
-  message("reading requirements file and existing lockfile")
-  requirements <- read_list(requirements_file)
-  lockfile     <- read_list(lockfile_path)
+install_reqs <- function(reqsfile = default_reqsfile_location(), lockfile = default_lockfile_location(), save_tree = TRUE) {
+  message("reading reqsfile and lockfile")
+  requirements <- read_list(reqsfile)
+  lockfile     <- read_list(lockfile)
 
   message("Checking if lockfile is up to date")
   lockbox_hash <- calculate_lockbox_hash(requirements)
   if (identical(lockbox_hash, lockfile$lockbox_hash)) {
-    message("Requirements unchanged, lockfile up to date")
+    message("Reqs unchanged, lockfile up to date")
   } else {
     message("Lockfile is outdated, recalculating dependencies")
     lockfile <- create_lockfile(requirements, lockbox_hash, lockfile_path, save_tree)
   }
-  
+
   install_from_lockfile(lockfile)
 }
--- R/utils.R
+++ R/utils.R
@@ -2,3 +2,8 @@ calculate_lockbox_hash <- function(requirements) {
   digest::digest(requirements)
 }
 
+default_reqsfile_location <- function() {
+}
+
+default_lockfile_location <- function() {
+}
