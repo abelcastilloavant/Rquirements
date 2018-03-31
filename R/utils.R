ca776d19c4b3b85cd6815cba6c3afc55bac7c147
Abel Castillo <abelcastillomath@gmail.com>
2018-03-31 17:25:21 -0500

    bring in common utilities

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,16 @@
+## A standard helper: if `x` is null, `y` will be returned instead.
+`%||%` <- function(x, y) if (is.null(x)) y else x
+
+## Easily support options being set as env vars with similar names
+get_option <- function(x, default = NULL) {
+  result <- getOption(x)
+  if (is.null(result)) {
+    result <- Sys.getenv(toupper(gsub("\\.", "_", x)))
+    if (!nzchar(result)) { result <- NULL }
+  }
+  result %||% default
+}
+
 rquirements_registry_path <- function() {
   ensure_directory_exists(get_option("rquirements.registry_path", "~/.R/rquirements/registry"))
 }
@@ -9,3 +22,4 @@ ensure_directory_exists <- memoise::memoise(function(directory) {
   }
   directory
 })
+
