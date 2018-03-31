364145d480978ee3b447dfec84f3ed09f8f046fc
Abel Castillo <abelcastillomath@gmail.com>
2018-03-31 15:49:44 -0500

    initial commit - better starting point

---
+++ R/load_requirements.R
@@ -0,0 +1,16 @@
+load_requirements <- function(reqsfile) {
+  reqs <- read_list(reqsfile)
+  if (`reqs_registered?`(reqs)) {
+    lockfile <- get_lockfile_from_registry(reqs)
+  } else {
+    lockfile <- generate_lockfile(reqs)
+    register_requirements(reqs, lockfile)
+  }
+
+  if (!`lockfile_registered?`(lockfile)) {
+    install_packages_into_lockbox(lockfile)
+    register_lockfile(lockfile)
+  }
+
+  set_session_package_versions(lockfile)
+}
--- R/utils.R
+++
@@ -1,5 +0,0 @@
-default_reqfile_location <- function() {
-}
-
-default_lockfile_location <- function() {
-}
